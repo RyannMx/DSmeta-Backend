1ed809e7e47e24c3092ca7c324ffa65e04838461
ryannmx <ryanshift22@gmail.com>
2022-07-13 22:21:54 -0300

    Sms Service

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,6 +18,8 @@ public class SaleController {
 	
 	@Autowired
 	private SaleService service;
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales( 
@@ -26,4 +29,8 @@ public class SaleController {
 		return service.findSales(minDate, maxDate, pageable);
 	}
 
+	@GetMapping("/notification")
+	public void notifySms() {
+		smsService.sendSms();
+	}
 }
---
+++ src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	public void sendSms() {
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, "Teste").create();
+
+		System.out.println(message.getSid());
+	}
+}
